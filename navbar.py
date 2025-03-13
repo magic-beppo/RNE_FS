8fe88de0d3c21d1aa02c3904b6f5501a7e1ebd3e
Josef Schmidhuber <schmidhuber.roma@gmail.com>
2025-03-13 19:54:49 +0100

    menu

--- navbar.py
+++ navbar.py
@@ -9,29 +9,31 @@ def Navbar():
             
             html.A([html.I(className="fas fa-home"), " Home"], href="https://morocco-menu-production.up.railway.app/", id='home-link', className="nav-link"),
             
-            html.A([html.I(className="fas fa-chart-line"), "Macro-WEO"], href="https://faomorocco8-production.up.railway.app/", id='Macro1', className="nav-link"),
+            html.A([html.I(className="fas fa-chart-line"), "Macro-WEO"], href="https://rnemacroimf-production.up.railway.app/", id='Macro1', className="nav-link"),
 
-            html.A([html.I(className="fas fa-chart-line"), "Macro-FAO"], href="https://faomorocco12-production.up.railway.app", id='Macro2', className="nav-link"),
+            html.A([html.I(className="fas fa-chart-line"), "Macro-FAO"], href="https://rnemacrofao-production.up.railway.app/", id='Macro2', className="nav-link"),
             
-            html.A([html.I(className="fas fa-chart-line"), "FS Indicators"], href="https://faomorocco2-production.up.railway.app/", id='SOI', className="nav-link"),
+            html.A([html.I(className="fas fa-chart-line"), "FS Indicators"], href="https://rnefs-production.up.railway.app/", id='SOI', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " SUA Balances"], href="https://psdmorocco-production.up.railway.app/", id='SUA', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " SUA Balances"], href="https://rnebalances-production.up.railway.app/", id='SUA', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Trade Analyst"], href="https://faomorocco3-production.up.railway.app/", id='Trade Analyst', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Trade Analyst"], href="https://rnetradeanalyst-production.up.railway.app/", id='Trade Analyst', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Trade Tracker"], href="https://faomorocco4-production.up.railway.app/", id='Tracker', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Food Trade Tracker"], href="https://rnefood-production.up.railway.app/", id='Tracker', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Food Import Bill"], href="https://faomorocco5-production.up.railway.app/", id='FIB', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Food Import Bill"], href="https://rnefib-production.up.railway.app/", id='FIB', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Virtual Water"], href="https://faomorocco6-production.up.railway.app/", id='Precipitation1', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Virtual Water"], href="https://rnevirtualwater-production.up.railway.app/", id='Precipitation1', className="nav-link"),
 
-            html.A([html.I(className="fas fa-database"), " Precipitation"], href="https://faomorocco9-production.up.railway.app//", id='Precipitation2', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Precipitation"], href="https://rneprecipitation-production.up.railway.app/", id='Precipitation2', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Crop Diversity"], href="https://faomorocco10-production.up.railway.app/", id='Diversity', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Crop Diversity"], href="https://rnedistributions-production.up.railway.app/", id='Diversity', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " Fertilizer"], href="https://faomorocco11-production.up.railway.app/", id='Fertilizer', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " Fertilizer"], href="https://rnefertilizer-production.up.railway.app/", id='Fertilizer', className="nav-link"),
             
-            html.A([html.I(className="fas fa-database"), " GAEZ"], href="https://faomorocco11-production.up.railway.app/", id='GAEZ', className="nav-link"),
+            html.A([html.I(className="fas fa-database"), " SDGs"], href="https://rnesdg-production.up.railway.app/", id='SDGs', className="nav-link"),
+            
+            html.A([html.I(className="fas fa-database"), " Trade Concentration"], href="https://rneconcentration-production.up.railway.app/", id='Concentraton', className="nav-link"),
             
         ],
     )
