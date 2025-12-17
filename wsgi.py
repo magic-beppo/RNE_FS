a78b460febadfffd9a4bc471add32720a5a4d4de
magic-beppo <schmidhuber.roma@gmail.com>
2025-12-17 14:46:30 +0100

    wgsi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # wsgi.py
 
-from Display_module_1_old import server  # <-- your Flask server exported in Tracker_mongo.py
+from Display_module_1 import server  # <-- your Flask server exported in Tracker_mongo.py
 
 # Some platforms look specifically for "application"
 application = server
